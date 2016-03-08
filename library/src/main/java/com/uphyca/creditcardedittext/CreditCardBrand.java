9afdb77e8ef0032ec90db8a8b5231d4428fc3c0b
Sosuke Masui <esmasui@gmail.com>
2016-03-08 17:22:17 +0900

    JCB, AMEXが最初の一文字の時点ではDinersと判定される不具合を修正した fixed #20

--- library/src/main/java/com/uphyca/creditcardedittext/CreditCardBrand.java
+++ library/src/main/java/com/uphyca/creditcardedittext/CreditCardBrand.java
@@ -2,27 +2,33 @@ package com.uphyca.creditcardedittext;
 
 import android.support.annotation.NonNull;
 
+import java.util.regex.Pattern;
+
 /**
  * カード会社に応じたカード番号情報
  * 参考: http://ja.wikipedia.org/wiki/ISO/IEC_7812
  * 参考: http://www.cardservice.co.jp/service/creditcard/csc.html
+ * 参考: https://en.wikipedia.org/wiki/Bank_card_number
  */
 public enum CreditCardBrand {
-    UNKNOWN(8, 19, new int[]{}), // Defined 'ISO IEC/7812'
-    VISA(13, 16, new int[]{4, 4, 4, 4}),
-    MASTER_CARD(16, 16, new int[]{4, 4, 4, 4}),
-    AMERICAN_EXPRESS(15, 15, new int[]{4, 6, 5}),
-    JCB(16, 16, new int[]{4, 4, 4, 4}),
-    DINERS_CLUB(14, 14, new int[]{4, 6, 4});
+
+    UNKNOWN(8, 19, "^$", new int[]{}), // Defined 'ISO IEC/7812'
+    VISA(13, 16, "^4.*", new int[]{4, 4, 4, 4}),
+    MASTER_CARD(16, 16, "^5[1-5].*", new int[]{4, 4, 4, 4}),
+    AMERICAN_EXPRESS(15, 15, "^3[47].*", new int[]{4, 6, 5}),
+    JCB(16, 16, "^35.*", new int[]{4, 4, 4, 4}),
+    DINERS_CLUB(14, 14, "^3[0689].*", new int[]{4, 6, 4});
 
     private final int minLength;
     private final int maxLength;
+    private final Pattern pattern;
     private final int[] format;
     private final int separatorCount;
 
-    CreditCardBrand(int minLength, int maxLength, int[] format) {
+    CreditCardBrand(int minLength, int maxLength, String pattern, int[] format) {
         this.minLength = minLength;
         this.maxLength = maxLength;
+        this.pattern = Pattern.compile(pattern);
         this.format = format;
         this.separatorCount = Math.max(0, format.length - 1);
     }
@@ -50,6 +56,16 @@ public enum CreditCardBrand {
         return separatorCount;
     }
 
+    /**
+     * 指定のカード番号がこのブランドのものかどうか判定する
+     *
+     * @param number カード番号
+     * @return ブランドに合致する場合はtrue、そうでない場合はfalse
+     */
+    public boolean matches(String number) {
+        return pattern.matcher(number).matches();
+    }
+
     public boolean isSeparatorPosition(int index) {
         int i = 0;
         for (int number : format) {
@@ -67,18 +83,12 @@ public enum CreditCardBrand {
 
     @NonNull
     public static CreditCardBrand getBrand(@NonNull String number) {
-        if (number.startsWith("4")) {
-            return VISA;
-        } else if (number.startsWith("5")) {
-            return MASTER_CARD;
-        } else if (number.startsWith("34") || number.startsWith("37")) {
-            return AMERICAN_EXPRESS;
-        } else if (number.startsWith("35")) {
-            return JCB;
-        } else if (number.startsWith("3")) {
-            return DINERS_CLUB;
+        for (CreditCardBrand each : CreditCardBrand.values()) {
+            boolean matches = each.matches(number);
+            if (matches) {
+                return each;
+            }
         }
         return UNKNOWN;
     }
-
 }
