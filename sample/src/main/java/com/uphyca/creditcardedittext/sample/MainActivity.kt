06b1c944567968d0befd49f18f947504418d8c07
Yuki Anzai <anzai.y.aa@gmail.com>
2020-02-20 16:14:40 +0900

    kotlinize sample

---
+++ sample/src/main/java/com/uphyca/creditcardedittext/sample/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.uphyca.creditcardedittext.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        numberEditText.addNumberListener { number, brand ->
+            brandView.text = "${brand.name} : $number"
+        }
+
+        dateEditText.addDateListener {
+            dateView.text = "year: ${it.year}, month : ${it.month}"
+        }
+    }
+}
